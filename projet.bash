4ea8065184288d47b9c494a0a8523e5628801a8e
mico81 <nicolas.pattyn72@gmail.com>
2015-01-27 14:35:03 +0100

    Jdjz

--- projet.bash
+++ projet.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+clesortie=1
+close=0
+
 echo " _____   _____   __   _       _   _____   _   _   _____   
 |  _  \ /  _  \ |  \ | |     | | /  _  \ | | | | |  _  \  
 | |_| | | | | | |   \| |     | | | | | | | | | | | |_| |  
@@ -7,9 +10,19 @@ echo " _____   _____   __   _       _   _____   _   _   _____
 | |_| | | |_| | | | \  | | |_| | | |_| | | |_| | | | \ \  
 |_____/ \_____/ |_|  \_| \_____/ \_____/ \_____/ |_|  \_\ "
 
+while [ "$close" -ne "$clesortie" ]
+do
+
 read commande parametre
 
-if [ $commande = "ping" ]
+if [ "$commande" = "ping" ]
 then
-        ping $parametre
+        ping -c 10 -i 0.2 $parametre -q | tail -n 1 | cut -d' ' -f4 | cut -d'/' -f2
 fi
+if [ "$commande" = "close" ]
+then
+	close=1
+fi
+done
+
+exit 0
